d726b1fb79e8c2ef343798f203262077bd3137bf
ahsanf <ahsanfirdaus.2018@student.uny.ac.id>
2019-12-04 01:16:27 +0700

    update detailview.php

--- detailview.php
+++ detailview.php
@@ -48,7 +48,7 @@ $pengetahuan            = "SELECT profile_lulusan.deskripsi,pengetahuan.deskrips
             <div class="container">
                 <div class="row">
                     <div class="col-3">
-                      <div class="nav nav-link flex-column" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                      <div class="nav nav-link flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                         <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Ketrampilan Umum</a>
                         <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Ketrampilan Khusus</a>
                         <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Sikap</a>
